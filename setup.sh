6ed1e951616ca40175677f193839b65a08833b8c
Snap888 <dolenor1@gmail.com>
2024-08-06 22:45:16 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,38 +1,26 @@
 #!/bin/bash
 
-# Установка необходимых пакетов
-if apt update && apt install -y git inotify-tools; then
-    echo "Пакеты успешно установлены."
-else
-    echo "Ошибка при установке пакетов."
-    exit 1
-fi
-
+# Установка git
+apt install -y git
+pkg install inotify-tools -y
 # Клонирование репозитория
 cd ~
-if git clone https://github.com/Snap888/Mobox-mod-wiget; then
-    echo "Репозиторий успешно клонирован."
-else
-    echo "Ошибка при клонировании репозитория."
-    exit 1
-fi
+git clone https://github.com/Snap888/Mobox-mod-wiget
 
 # Создание директорий
-mkdir -p "$HOME/tmp" && echo "Создана директория tmp."
-mkdir -p "$HOME/Desktop" && echo "Создана директория Desktop."
-mkdir -p /data/data/com.termux/files/home/.shortcuts && echo "Создана директория .shortcuts."
-chmod 700 -R /data/data/com.termux/files/home/.shortcuts && echo "Права доступа к .shortcuts изменены."
-mkdir -p /data/data/com.termux/files/home/.shortcuts/tasks && echo "Создана директория tasks."
-chmod 700 -R /data/data/com.termux/files/home/.shortcuts/tasks && echo "Права доступа к tasks изменены."
+mkdir -p "$HOME/tmp"
+mkdir -p "$HOME/Desktop"
+mkdir -p /data/data/com.termux/files/home/.shortcuts
+chmod 700 -R /data/data/com.termux/files/home/.shortcuts
+mkdir -p /data/data/com.termux/files/home/.shortcuts/tasks
+chmod 700 -R /data/data/com.termux/files/home/.shortcuts/tasks
 
-# Выбор опции установки
 echo "Опции выбора"
 echo "1) Установить Mod WEG™ для mobox box86"
 echo "2) Установить Мод WEG™ для mobox WoW64"
 echo ""
 echo -n "Введите номер: "
 read i
-
 case "$i" in
     1)
         MOBOX=X86
@@ -41,43 +29,62 @@ case "$i" in
         MOBOX=WoW64
         ;;
     *)
-        echo "Неверный выбор. Завершение."
         exit 1
         ;;
 esac
 
 echo "Установка WEG™ mod"
 
-# Копирование файлов в зависимости от выбора пользователя
 if [ "$MOBOX" = "WoW64" ]; then
-    cp "$HOME/Mobox-mod-wiget/auto-link-kreator" "$PREFIX/bin/" && echo "Копирован auto-link-kreator."
-    cp "$HOME/Mobox-mod-wiget/link-source-code-run" "$PREFIX/bin/" && echo "Копирован link-source-code-run."
-    cp "$HOME/Mobox-mod-wiget/weg-mod" "$PREFIX/glibc/opt/scripts/" && echo "Копирован weg-mod."
-    cp "$HOME/Mobox-mod-wiget/weg-mod-menu" "$PREFIX/glibc/opt/scripts/" && echo "Копирован weg-mod-menu."
-    cp "$HOME/Mobox-mod-wiget/weg-mod-start" "$PREFIX/glibc/opt/scripts/" && echo "Копирован weg-mod-start."
-else
-    cp "$HOME/Mobox-mod-wiget/auto-link-kreator" "$PREFIX/bin/" && echo "Копирован auto-link-kreator."
-    cp "$HOME/Mobox-mod-wiget/link-source-code-run-x86" "$PREFIX/bin/" && echo "Копирован link-source-code-run-x86."
-    cp "$HOME/Mobox-mod-wiget/weg-mod-x86" "$PREFIX/glibc/opt/scripts/" && echo "Копирован weg-mod-x86."
-    cp "$HOME/Mobox-mod-wiget/weg-mod-menu-x86" "$PREFIX/glibc/opt/scripts/" && echo "Копирован weg-mod-menu-x86."
-    cp "$HOME/Mobox-mod-wiget/weg-mod-start-x86" "$PREFIX/glibc/opt/scripts/" && echo "Копирован weg-mod-start-x86."
+    # Копирование файлов
+    cp "$HOME/Mobox-mod-wiget/auto-link-kreator" /data/data/com.termux/files/home/bin/
+    cp "$HOME/Mobox-mod-wiget/link-source-code-run" /data/data/com.termux/files/home/bin/
+    cp "$HOME/Mobox-mod-wiget/weg-mod" /data/data/com.termux/files/home/glibc/opt/scripts/
+    cp "$HOME/Mobox-mod-wiget/weg-mod-menu" /data/data/com.termux/files/home/glibc/opt/scripts/
+    cp "$HOME/Mobox-mod-wiget/weg-mod-start" /data/data/com.termux/files/home/glibc/opt/scripts/
+    cp "$HOME/Mobox-mod-wiget/watch_script.log" "$HOME/tmp/"
+
+    # Создание символической ссылки
+    ln -sf /data/data/com.termux/files/home/glibc/opt/scripts/weg-mod /data/data/com.termux/files/home/bin/weg-mod
+
+    # Изменение прав доступа на файлы
+    chmod +x /data/data/com.termux/files/home/bin/auto-link-kreator
+    chmod +x /data/data/com.termux/files/home/bin/link-source-code-run
+    chmod +x /data/data/com.termux/files/home/glibc/opt/scripts/weg-mod
+    chmod +x /data/data/com.termux/files/home/glibc/opt/scripts/weg-mod-menu
+    chmod +x /data/data/com.termux/files/home/glibc/opt/scripts/weg-mod-start
+    chmod +x "$HOME/tmp/watch_script.log"
+    chmod +x /data/data/com.termux/files/home/bin/weg-mod
+
+    # Удаление папки Mobox-mod-wiget
+    rm -rf "$HOME/Mobox-mod-wiget"
+
+    echo "Все операции завершены успешно."
 fi
 
-cp "$HOME/Mobox-mod-wiget/watch_script.log" "$HOME/tmp/" && echo "Копирован watch_script.log."
+if [ "$MOBOX" = "X86" ]; then
+    # Копирование файлов
+    cp "$HOME/Mobox-mod-wiget/auto-link-kreator" /data/data/com.termux/files/home/bin/auto-link-kreator
+    cp "$HOME/Mobox-mod-wiget/link-source-code-run-x86" /data/data/com.termux/files/home/bin/link-source-code-run
+    cp "$HOME/Mobox-mod-wiget/weg-mod-x86" /data/data/com.termux/files/home/glibc/opt/scripts/weg-mod
+    cp "$HOME/Mobox-mod-wiget/weg-mod-menu-x86" /data/data/com.termux/files/home/glibc/opt/scripts/weg-mod-menu
+    cp "$HOME/Mobox-mod-wiget/weg-mod-start-x86" /data/data/com.termux/files/home/glibc/opt/scripts/weg-mod-start
+    cp "$HOME/Mobox-mod-wiget/watch_script.log" "$HOME/tmp/"
 
-# Создание символической ссылки
-ln -sf "$PREFIX/glibc/opt/scripts/weg-mod" "$PREFIX/bin/weg-mod" && echo "Создана символическая ссылка weg-mod."
+    # Создание символической ссылки
+    ln -sf /data/data/com.termux/files/home/glibc/opt/scripts/weg-mod /data/data/com.termux/files/home/bin/weg-mod
 
-# Изменение прав доступа на файлы
-chmod +x "$PREFIX/bin/auto-link-kreator" && echo "Права доступа для auto-link-kreator изменены."
-chmod +x "$PREFIX/bin/link-source-code-run" && echo "Права доступа для link-source-code-run изменены."
-chmod +x "$PREFIX/glibc/opt/scripts/weg-mod" && echo "Права доступа для weg-mod изменены."
-chmod +x "$PREFIX/glibc/opt/scripts/weg-mod-menu" && echo "Права доступа для weg-mod-menu изменены."
-chmod +x "$PREFIX/glibc/opt/scripts/weg-mod-start" && echo "Права доступа для weg-mod-start изменены."
-chmod +x "$HOME/tmp/watch_script.log" && echo "Права доступа для watch_script.log изменены."
-chmod +x "$PREFIX/bin/weg-mod" && echo "Права доступа для weg-mod изменены."
+    # Изменение прав доступа на файлы
+    chmod +x /data/data/com.termux/files/home/bin/auto-link-kreator
+    chmod +x /data/data/com.termux/files/home/bin/link-source-code-run
+    chmod +x /data/data/com.termux/files/home/glibc/opt/scripts/weg-mod
+    chmod +x /data/data/com.termux/files/home/glibc/opt/scripts/weg-mod-menu
+    chmod +x /data/data/com.termux/files/home/glibc/opt/scripts/weg-mod-start
+    chmod +x "$HOME/tmp/watch_script.log"
+    chmod +x /data/data/com.termux/files/home/bin/weg-mod
 
-# Удаление папки Mobox-mod-wiget
-rm -rf "$HOME/Mobox-mod-wiget" && echo "Папка Mobox-mod-wiget удалена."
+    # Удаление папки Mobox-mod-wiget
+    rm -rf "$HOME/Mobox-mod-wiget"
 
-echo "Все операции завершены успешно."
+    echo "Все операции завершены успешно."
+fi
