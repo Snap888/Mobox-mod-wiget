ae25e579932bfb0e726b39ea651cf8bb45b57627
Snap888 <dolenor1@gmail.com>
2024-08-06 11:25:27 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,35 +21,3 @@ pkg install ncurses-utils -y &>/dev/null
 pkg install hashdeep -y &>/dev/null
 pkg install termux-x11-nightly -y &>/dev/null
 
-if [ -e $PREFIX/glibc ]; then
-    echo -n "Removing previous glibc. Continue? (Y/n) "
-    read i
-    if [ "$i" = "Y" ] || [ "$i" = "y" ]; then
-        rm -rf $PREFIX/glibc
-    else
-        return 1
-    fi
-fi
-
-INSTALL_WOW64=0
-
-echo "Select an option"
-echo "1) Install previous mobox with box86"
-echo "2) Install new mobox wow64 version"
-echo ""
-echo -n "Selected number: "
-read i
-case "$i" in
-1)
-    INSTALL_WOW64=0
-;;
-2)
-    INSTALL_WOW64=1
-;;
-*)
-    return 1
-;;
-esac
-
-echo "Installing mobox"
-
